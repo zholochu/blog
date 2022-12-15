bff9db394389927321527c0642495977edb39b18
Arstanbek Zholochu <arstanbek.zholochu@gmail.com>
2022-12-15 11:29:05 -0500

    Added basic authentification

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+  http_basic_authenticate_with name: "Arstan", password: "secret", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
