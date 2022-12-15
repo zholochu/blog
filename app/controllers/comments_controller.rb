fc8c5cd8116055949ee1aa9d588b6e8876357a79
Arstanbek Zholochu <arstanbek.zholochu@gmail.com>
2022-12-15 10:58:03 -0500

    Modified DB by adding column status to articles and comments, added concern

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
 
   private
 
-    def comment_params
-      params.require(:comment).permit(:commenter, :body)
-    end
+  def comment_params
+    params.require(:comment).permit(:commenter, :body, :status)
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,12 @@ class Article < ApplicationRecord
   has_many :comments
   validates :title, presence: true
   validates :body, presence: true
+
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived
+    status == 'archived'
+  end
 end
