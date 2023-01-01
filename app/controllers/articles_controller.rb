40208a01ed4c6bff89a2b7e603fbab726499a2d4
Arstanbek Zholochu <arstanbek.zholochu@gmail.com>
2023-01-01 15:12:58 -0500

    Created User model for Devise

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
 
-  http_basic_authenticate_with name: "Arstan", password: "secret", except: [:index, :show]
+  # http_basic_authenticate_with name: "Arstan", password: "secret", except: [:index, :show]
 
   def index
     @articles = Article.all
